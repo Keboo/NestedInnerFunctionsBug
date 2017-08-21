f66b2246784fd0fe08a3118939f729e2a69acdb9
Kevin Bost <kevin@IntelliTect.com>
2017-08-21 14:15:34 -0700

    Initial check-in

---
+++ NestedInnerFunctionsBug/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NestedInnerFunctionsBug
+{
+    public class Class1
+    {
+        private void Test()
+        {
+            bool outside = true;
+
+            void Inner() //This can also be a lambda (ie. Action action = () => { ... };)
+            {
+                void Bar()
+                {
+                }
+
+                void Foo()
+                {
+                    Bar();
+
+                    bool captured = outside;
+                }
+            }
+        }
+    }
+}
